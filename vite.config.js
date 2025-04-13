2c68b90ed6afb46ba0005284dbdcb932e929202a
Thon <137491012+thon1525@users.noreply.github.com>
2025-04-14 00:40:47 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://wims-z0uz.onrender.com',
+      '/api': 'https://wims-z0uz.onrender.com/',
     },
   },
 });
